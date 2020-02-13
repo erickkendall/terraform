d1701da473bdb0581b5e7f7ae5f7365f11b9648c
Erick Kendall <ekendall@franklinamerican.com>
2020-02-13 14:31:22 -0600

    First commit.

---
+++ demo-1/vars.tf
@@ -0,0 +1,18 @@
+variable "AWS_ACCESS_KEY" {
+
+}
+variable "AWS_SECRET_KEY" {
+
+}
+variable "AWS_REGION" {
+  default = "us-east-1"
+
+}
+variable "AMIS" {
+  type = "map"
+  default = {
+    "us-east-1" = "ami-046842448f9e74e7d"
+    "us-east-2" = "ami-0367b500fdcac0edc"
+  }
+}
+
