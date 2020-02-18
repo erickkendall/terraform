fa22c1201492cc7844a0bc65d477f21a8df4d851
erickkendall <ekendall0013@gmail.com>
2020-02-18 06:45:20 -0600

    add wp-config.php for WordPress

--- demo-2a/script.sh
+++ demo-2a/script.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/bash
-yum update -y
 yum install httpd php php-mysql -y
 cd /var/www/html
-wget https://wordpress.org/latest.zip
-unzip latest.zip
-cp -rp wordpress/* /var/www/html/
+wget https://wordpress.org/wordpress-5.1.1.tar.gz
+tar -xzf wordpress-5.1.1.tar.gz
+cp -r wordpress/* /var/www/html/
 rm -rf wordpress
-rm -rf latest.zip
+rm wordpress-5.1.1.tar.gz
 chmod -R 755 wp-content
 chown -R apache:apache wp-content
 service httpd start
