e5bf025f080e791a372d3756f482efd0060eb593
erickkendall <ekendall0013@gmail.com>
2020-02-19 07:39:01 -0600

    demo use of terraform data

--- demo-5/securitygroup.tf
+++ demo-5/securitygroup.tf
@@ -1,7 +1,8 @@
 data "aws_ip_ranges" "us_ec2" {
-  regions  = ["us-east-1", "us-east-2"]
+  regions  = ["us-east-1"]
   services = ["ec2"]
 }
+
 resource "aws_security_group" "from_us" {
   name = "from_us"
 
@@ -9,10 +10,10 @@ resource "aws_security_group" "from_us" {
     from_port   = "443"
     to_port     = "443"
     protocol    = "tcp"
-    cidr_blocks = [data.aws_ip_ranges.us_ec2.cidr_blocks]
+    cidr_blocks =  data.aws_ip_ranges.us_ec2.cidr_blocks 
   }
-  tags {
-    CreateDate = data.aws_ip_ranges.us_ec2.cidr_blocks
-    SyncToken  = data.aws_ip_ranges.us_ec2.sync_token
+  tags = {
+    CreateDate = "${data.aws_ip_ranges.us_ec2.create_date}"
+    SyncToken  = "${data.aws_ip_ranges.us_ec2.sync_token}"
   }
 }
