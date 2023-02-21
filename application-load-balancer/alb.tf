05e064e44cd6bc9ff2e7556c180703cb3b8f1a43
erickkendall <ekendallgithub@gmail.com>
2023-02-21 07:38:22 -0600

    latest

--- application-load-balancer/alb.tf
+++ application-load-balancer/alb.tf
@@ -1,39 +1,63 @@
-resource "aws_lb" "example" {
-  name               = "example-alb"
+resource "aws_lb" "comcast" {
+  name               = "comcast-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.sg.id]
   subnets            = aws_subnet.public_subnet.*.id
 }
-resource "aws_lb_listener" "example" {
-  load_balancer_arn = aws_lb.example.arn
-  port              = 80
+resource "aws_lb_listener" "http_redirect" {
+  load_balancer_arn = aws_lb.comcast.arn
+  port              = "80"
   protocol          = "HTTP"
 
+
+
   default_action {
-    target_group_arn = aws_lb_target_group.example.arn
-    type             = "forward"
+    type = "redirect"
+    redirect {
+      protocol    = "HTTPS"
+      port        = "443"
+      status_code = "HTTP_301"
+    }
   }
 }
-resource "aws_lb_target_group" "example" {
+resource "aws_lb_listener" "example" {
+  load_balancer_arn = aws_lb.comcast.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-Ext-2018-06"
+  certificate_arn   = aws_iam_server_certificate.test_cert.arn
+
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.comcast.arn
+  }
+}
+resource "aws_lb_target_group" "comcast" {
   name_prefix = "tg-ex"
-  port        = 80
-  protocol    = "HTTP"
+  port        = 443
+  protocol    = "HTTPS"
   vpc_id      = aws_vpc.custom_vpc.id
 
   health_check {
     healthy_threshold   = 2
     interval            = 30
     matcher             = "200"
-    path                = "/"
-    port                = "traffic-port"
-    protocol            = "HTTP"
+    path                = "/index.html"
+#    port                = "traffic-port"
+    port                = 80
+    protocol            = "HTTPS"
     timeout             = 5
     unhealthy_threshold = 2
   }
 }
 resource "aws_alb_target_group_attachment" "tgattachment" {
   count            = length(aws_instance.instance.*.id) == 3 ? 3 : 0
-  target_group_arn = aws_lb_target_group.example.arn
+  target_group_arn = aws_lb_target_group.comcast.arn
   target_id        = element(aws_instance.instance.*.id, count.index)
 }
+#resource "aws_lb_listener_certificate" "example" {
+#  listener_arn    = aws_lb_listener.example.arn
+#  certificate_arn = aws_iam_server_certificate.test_cert.arn
+#}
--- application-load-balancer/cert.tf
+++ application-load-balancer/cert.tf
@@ -1,9 +1,8 @@
-resource "tls_private_key" "example_key" {
+resource "tls_private_key" "example_cert" {
   algorithm = "RSA"
-  rsa_bits  = 4096
 }
 resource "tls_self_signed_cert" "example" {
-  private_key_pem = tls_private_key.example_key.private_key_pem
+  private_key_pem = tls_private_key.example_cert.private_key_pem
 
   subject {
     common_name  = "example.com"
@@ -17,4 +16,17 @@ resource "tls_self_signed_cert" "example" {
     "digital_signature",
     "server_auth",
   ]
+}
+
+resource "aws_iam_server_certificate" "test_cert" {
+  name             = "some_test_cert"
+  certificate_body = tls_self_signed_cert.example.cert_pem
+  private_key      = tls_private_key.example_cert.private_key_pem
+
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.example.private_key_pem}' > ./private_key.pem"
+  }
+  provisioner "local-exec" {
+    command = "echo '${aws_iam_server_certificate.test_cert.certificate_body}' > ./cert.pem"
+  }
 }
